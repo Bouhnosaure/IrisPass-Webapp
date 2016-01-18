000f4bca8f17083102214e89b6353845162c9cf6
alex <alexandre.mangin@viacesi.fr>
2016-01-18 19:20:43 +0100

    Remove all + move 5.2

--- tests/api/App/AppCept.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$I = new ApiTester($scenario);
-$I->wantTo('know is app is alive');
-
-$I->sendGET('/', []);
-
-$I->seeResponseCodeIs(200);
--- tests/api/Security/AccessProtectedNoAuthCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$I = new ApiTester($scenario);
-$I->wantTo('Access a protected resource without token');
-
-$I->sendGET('test/secure');
-
-$I->seeResponseCodeIs(401);
-$I->seeResponseIsJson();
